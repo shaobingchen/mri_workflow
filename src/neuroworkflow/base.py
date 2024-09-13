b2cee0d5b4ef92e9a0d686b3660ef8bcb904c6cf
sbc <2449825766@qq.com>
2024-09-13 18:46:54 +0800

    rewrite work_directed_graph now can handle works share component in both input and output
    rewrite _skip and overwrite strategy
    change input_components to input_component_list and input_component_set
    change output_components to output_component_list and output_component_set
    add environment support in Commandwork

--- src/neuroworkflow/base.py
+++ src/neuroworkflow/base.py
@@ -1,6 +1,3 @@
-'''
-carefully use skip_exist when your workflow contain works directly act on the input_components, this may cause rerun of the have processed components 
-'''
 import os 
 import os.path as op
 import networkx as nx
@@ -35,6 +32,9 @@ class RunMetaData(object):
         self.name_type = name_type
         self._current_format = None #do not use this explicitly, will change when commponent using as different work's input
         self.intial = True
+        
+        if skip_exist and overwrite:
+            raise ValueError("skip_exist and overwrite can't both be True")
         
         _logger = logging.getLogger(logger)
         _logger.info(f"create RunMetaData with\n rootdir {rootdir}\n subject {subject}\n session {session}\n logger {logger}\n overwrite {overwrite}\n preview {preview}")
@@ -243,11 +243,22 @@ class Work(object):
         
         self.name = name
                       
-        if self._test_component_list(input_components):
-            self.input_components = input_components
+        if isinstance(input_components,list):
+            self.input_components_list = input_components
+            self.input_components_set = set(input_components)
+        elif isinstance(input_components,set):
+            self.input_components_set = input_components
+        else:
+            raise ValueError(f"input_components of {self.name} should be a list or set")
+        
+        if isinstance(output_components, list):
+            self.output_components_list = output_components
+            self.output_components_set = set(output_components)
+        elif isinstance(output_components, set):
+            self.output_components_set = output_components
+        else:
+            raise ValueError(f"output_components of {self.name} should be a list or set")
                     
-        if self._test_component_list(output_components):
-            self.output_components = output_components
             
         self.action = action
         
@@ -284,7 +295,7 @@ class Work(object):
                 
     @property    
     def all_components(self) -> set:
-        return set(self.input_components + self.output_components)
+        return self.input_components_set | self.output_components_set
     
     def _pre_run(self, run_metadata):
         
@@ -295,15 +306,15 @@ class Work(object):
         logger = logging.getLogger(run_metadata.logger)
         logger.info(f"run {self.name}, work_heap is {run_metadata._work_heap}")
         
-        if len(self.input_components) == 0:
-            logger.warning(f"list of input_components {self.name} is empty.")
+        if not self.input_components_set:
+            logger.error(f"list of input_components {self.name} is empty.")
         
-        for index, component in enumerate(self.input_components):
+        for index, component in enumerate(self.input_components_list):
             
             if self.input_format is None:
                 pass
-            elif len(self.input_format) != len(self.input_components):
-                raise ValueError(f"input format {self.input_format} of {self.name} don't match the input components {self.input_components}")     
+            elif len(self.input_format) != len(self.input_components_list):
+                raise ValueError(f"input format {self.input_format} of {self.name} don't match the input components {self.input_components_list}")     
             else: 
                 logger.debug(f"set input component {component.simplified_bids_name} 's format as {self.input_format[index]}")          
                 component.run_metadata._current_format = self.input_format[index]
@@ -311,17 +322,17 @@ class Work(object):
             if not op.exists(component.use_name()):
                 raise ValueError(f"input component {component.use_name()} of work {self.name} does not exist")    
             
-        if len(self.input_components) == 0:
-            logger.warning(f"list of output_components {self.name} is empty, eventhough this work update component in input_components and don't generate new file, it should be added to output_components")    
+        if not self.output_components_set:
+            logger.error(f"list of output_components {self.name} is empty, eventhough this work update component in input_components and don't generate new file, it should be added to output_components")    
                         
-        for component in self.output_components:
+        for component in self.output_components_set:
             transform_run_metadata = dc(run_metadata)
             component.run_metadata = transform_run_metadata
             
             if self.output_format is None:
                 pass
-            elif len(self.output_format) != len(self.output_components):
-                raise ValueError(f"output format {self.output_format} of {self.name} don't match the output components {self.output_components}")
+            elif len(self.output_format) != len(self.output_components_list):
+                raise ValueError(f"output format {self.output_format} of {self.name} don't match the output components {self.output_components_list}")
             else:
                 logger.debug(f"set output component {component.simplified_bids_name} 's format as {self.output_format[index]}")
                 component.run_metadata._current_format = self.output_format[index]
@@ -331,19 +342,25 @@ class Work(object):
                 logger.warning(f"create directory {component.run_dir()}")
         
         _all_output_component_exist = True        
-        for component in self.output_components:
-            if op.exists(component.use_name()):                
-                logger.warning(f"file {component.use_name()} exist before running.")
-                if component not in self.input_components and run_metadata.overwrite:      
-                    component.remove_file()
-                    logger.warning(f"remove pre-exist file {component.use_name()} because overwrite has been setted")
+        for component in self.output_components_set:
+            
+            if op.exists(component.use_name()):                         
+                if component not in self.input_components_set: 
+                    
+                    logger.warning(f"file {component.use_name()} exist before running, and it not in input components.") 
+                    if run_metadata.overwrite: 
+                        
+                        component.remove_file()
+                        logger.warning(f"remove pre-exist file {component.use_name()} because overwrite has been setted")
             
             else:
                 _all_output_component_exist = False
+
                 
-        if  _all_output_component_exist:
+        if  _all_output_component_exist and not self.output_components_set.issubset(self.input_components_set):
             run_metadata._skip = True
-            logger.warning(f"skip running {self.name} because all output components are exist, if a output is also a exist input, this may skip a undo work")    
+            logger.warning(f"skip running {self.name} because all output components are exist, if a output is also a exist input, this may redo a work") 
+           
                     
             
 
@@ -366,7 +383,7 @@ class Work(object):
             
         if run_metadata.preview:
                             
-            for component in self.output_components:
+            for component in self.output_components_set:
                 
                 component.make_test_file()
                 logger.info(f"make test file {component.use_name()}")
@@ -375,9 +392,9 @@ class Work(object):
             
             def _process_item(self, item):
                 if isinstance(item, Component):
-                    if item in self.input_components:
+                    if item in self.input_components_set:
                         return item.use_name()
-                    elif item in self.output_components:
+                    elif item in self.output_components_set:
                         return item.use_name()
                     else:
                         raise ValueError(f"component {item.simplified_bids_name} of {self.name} is not in either input_components or output_components")
@@ -409,10 +426,14 @@ class Work(object):
                         name_surfix, final_surfix = item[-2], item[-1]
                         
                     return item[component_position].name_for_run(name_prefix = name_prefix, name_surfix = name_surfix, final_prefix = final_prefix, final_surfix = final_surfix)
+                
+                elif isinstance(item, (int, float, complex)):
+                    
+                    return str(item)
                                                             
                     
                 else:
-                    raise ValueError(f"item {item} of {self.name} is not a Component or a string")
+                    raise ValueError(f"item {item} of {self.name} is not a Component, string, list or number")
             
             
             
@@ -427,8 +448,8 @@ class Work(object):
                               
         elif 'run_metadata' in inspect.signature(self.action).parameters:
 
-            _run_input_components = [component.use_name() for component in self.input_components]
-            _run_output_components = [component.use_name() for component in self.output_components]
+            _run_input_components = [component.use_name() for component in self.input_components_set]
+            _run_output_components = [component.use_name() for component in self.output_components_set]
             
             logger.info(f"start running action {self.action.__name__} {_run_input_components, _run_output_components} of work {self.name} with run metadata")
             
@@ -446,8 +467,8 @@ class Work(object):
                                 
         else:
             
-            _run_input_components = [component.use_name() for component in self.input_components]
-            _run_output_components = [component.use_name() for component in self.output_components]
+            _run_input_components = [component.use_name() for component in self.input_components_list]
+            _run_output_components = [component.use_name() for component in self.output_components_list]
             
             logger.info(f"start running action {self.action.__name__} {_run_input_components, _run_output_components} of work {self.name}")
             
@@ -473,7 +494,7 @@ class CommandWork(Work):
     class to wrap command line as a work
     '''
     
-    def __init__(self, name, input_components=None, output_components=None, command_list = None, save_stdout_to = None, stdout_to_log = True, **kwargs):
+    def __init__(self, name, input_components=None, output_components=None, command_list = None, save_stdout_to = None, stdout_to_log = True, env = None, **kwargs):
         
         super().__init__(name, input_components, output_components, self._run_shell_command, **kwargs)
         if command_list is None:
@@ -482,6 +503,12 @@ class CommandWork(Work):
             self.command_list = command_list
         self.save_stdout_to = save_stdout_to
         self.stdout_to_log = stdout_to_log
+        
+        if env is None:
+            self.env = None
+        else:
+            self.env = os.environ.copy()
+            self.env.update(env)
         
     def _run_shell_command(self, command_list: list, run_metadata: RunMetaData):
     
@@ -495,7 +522,8 @@ class CommandWork(Work):
                 command_list,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,  # Capture stderr separately
-                text=True  # Return strings instead of bytes
+                text=True,  # Return strings instead of bytes
+                env=self.env
             )
             
             # Wait for the process to complete and capture output
@@ -585,8 +613,8 @@ class Workflow(Work):
         
     def add_work(self, work):
         self.worklist.append(work)
-        self.input_components.update(work.input_components)
-        self.output_components.update(work.output_components)
+        self.input_components_set.update(work.input_components_set)
+        self.output_components_set.update(work.output_components_set)
     
     @property    
     def cp_directed_graph(self):
@@ -597,7 +625,7 @@ class Workflow(Work):
         G.add_nodes_from(self.all_components)
         
         for work in self.worklist:
-            G.add_edges_from(product(work.input_components, work.output_components))
+            G.add_edges_from(product(work.input_components_set, work.output_components_set))
         
         return G
     
@@ -615,23 +643,44 @@ class Workflow(Work):
         '''
         create a directed graph with work as nodes and components as edges
         components are contained in the components attribute of the edge
+        if a work's input_component exist in multiple output_component_list of other works, if they are in the same branch, only keep the last one, if they are not in the same branch, conflict occur and raise error
         '''
-        G = nx.DiGraph()
-        
+        directed_graph = nx.DiGraph()
+            
+        ancestor_test = return_ancestor_test(directed_graph)
+        from functools import cmp_to_key
+                
         for work in self.worklist:
-            G.add_node(work)
-            #TODO may exchange the order work.input_components and G.nodes, to pre-construct a dictinary for the input_components to accelerate the process
-            for input_component in work.input_components:
-                for already_in_work in G.nodes:
-                    matching_components = [
-                        output_component
-                        for output_component in already_in_work.output_components
+            directed_graph.add_node(work)
+            
+            for input_component in work.input_components_set:
+                
+                matched_works = set()
+                
+                for already_in_work in directed_graph.nodes:
+                    matched_works = [
+                        already_in_work
+                        for output_component in already_in_work.output_components_set
                         if input_component == output_component
                     ]
-                    if matching_components:
-                        G.add_edge(already_in_work, work, components=matching_components)
+                                
+                    if len(matched_works) == 0:
+                        raise ValueError(f"input component {input_component.simplified_bids_name} of work {work.name} is not in any output components of any work")
+                    elif len(matched_works) == 1:
+                        matched_work = matched_works[0]
+                    elif len(matched_works) > 1: #test if they are in the same branch, if not, they are conflict, if yes,only keep the last one
+                        matched_works.sort(key = cmp_to_key(ancestor_test))
+                        print(f"input component {input_component.simplified_bids_name} of work {work.name} is in output components of multiple works {[matched_work.name for matched_work in matched_works]} in same branch, only keep the last one {matched_works[-1].name}")
+                        matched_work = matched_works[-1]
+                        
+                if directed_graph.has_edge(matched_works, work):
+                    directed_graph[matched_work][work]['components'].add(input_component)
+                else:
+                    directed_graph.add_edge(matched_work, work, components = {input_component})           
+                           
+
         
-        return G
+        return directed_graph
     
     @property
     def get_output_components(self):
@@ -639,14 +688,14 @@ class Workflow(Work):
         get output components of a workflow
         this will return a set of components that are the output of any work that contains the workflow 
         '''
-        return {component for work in self.work_list for component in work.output_components}
+        return set().union(*(work.output_components_set for work in self.work_list))
     
     @property
     def get_input_components(self):
         ''' 
         get input components of a workflow
         '''
-        all_input_component = {component for work in self.work_list for component in work.input_components}
+        all_input_component = set().union(*(work.input_components_set for work in self.work_list))
         
         return all_input_component.difference(self.get_output_components)
         
@@ -670,7 +719,9 @@ class Workflow(Work):
         for work in self.work_list:  
             
             transfor_run_metadata = dc(run_metadata) 
-            work.run(transfor_run_metadata)  
+            work.run(transfor_run_metadata)
+        
+        logger.info(f"finish running workflow {self.name}")  
                 
     
     @property
@@ -692,14 +743,25 @@ class Workflow(Work):
         decorator to update input components
         '''
         def wrapper(self, *args, **kwargs):
-            self.input_components = self.get_input_components
+            self.input_components_set = self.get_input_components_set
             return func(self, *args, ** kwargs)
         return wrapper
 
         
         
         
-        
+def return_ancestor_test(graph: nx.DiGraph):
+    if not isinstance(graph, nx.DiGraph):
+        raise ValueError("graph should be a DiGraph")
+    
+    def _ancestor_test(work1, work2):
+        if nx.has_path(graph, work2, work1):
+            return 1
+        elif nx.has_path(graph, work1, work2):
+            return -1
+        else:
+            raise ValueError(f"work {work1.name} and work {work2.name} are not in the same branch, conflict")                
+    return _ancestor_test        
 
 
 
